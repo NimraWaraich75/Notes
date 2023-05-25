e8c37df233a48e5666afb93dd80e0cc3b70a6477
NimraWaraich75 <waleedwaraich75@gmail.com>
2023-05-25 17:32:47 +0500

    library published

--- app/src/main/java/com/example/notesact/MainActivity.kt
+++ app/src/main/java/com/example/notesact/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.notesact
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteslibrary.ToastForNotes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastForNotes().showToast(this,"lib")
+
     }
 }
